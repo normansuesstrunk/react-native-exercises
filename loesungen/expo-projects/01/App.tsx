d1bcb4faf35a86a352c694d673c760bf80be0b3c
Norman Süsstrunk <n.suesstrunk@gmail.com>
2026-09-07 16:22:19 +0200

    added expo projects solutions

--- loesungen/expo-projects/01/App.tsx
+++ loesungen/expo-projects/01/App.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-type MyProps = {
-  name: string;
-};
-
-const Greeting = (p: MyProps) => {
-  return <Text style={styles.greeting}>Hallo, {p.name}!</Text>;
-};
-
-
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TextInput } from 'react-native';
 
 export default function App() {
+  const [text, setText] = useState('');
+
   return (
     <View style={styles.container}>
-      <Greeting name="Max" />
-      <Greeting name="Anna" />
-      <Greeting name="Peter" />
+      <TextInput
+        style={styles.input}
+        placeholder="Tippe hier zum Übersetzen!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={styles.pizza}>
+        {text.split(' ').map(word => word && '🍕').join(' ')}
+      </Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 24, gap: 12 },
-  greeting: { fontSize: 20 },
+  input: { height: 44, borderWidth: 1, borderColor: '#999', borderRadius: 8, paddingHorizontal: 12 },
+  pizza: { fontSize: 36 },
 });
